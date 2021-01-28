89b1bf60669e05286b7a421d114ca995a52d7bb0
rl538 <rui.li.rl538@yale.edu>
2021-01-28 17:26:19 -0500

    Avoid using string and algorithm libraries.
    o/w dflowmap may not compile properly.

--- lib.cc
+++ lib.cc
@@ -1,68 +1,55 @@
 #include <stdio.h>
-#include <string.h>
-#include <vector>
-#include <algorithm>
-#include <act/act.h>
 
-typedef std::string String;
-typedef std::vector<String> StringVec;
-StringVec createdProcs;
+#define MAX_TYPE_NUM 100
 
-void createBinLib(FILE *libFp, const char *sym, const char *op) {
-  String symbol(sym);
-  if (std::find(createdProcs.begin(), createdProcs.end(), symbol)
-      == createdProcs.end()) {
-    createdProcs.push_back(symbol);
+int processedTypes[MAX_TYPE_NUM] = {-1};
+
+bool hasType(int typeId) {
+  for (unsigned i = 0; i < MAX_TYPE_NUM; i++) {
+    if (processedTypes[i] == typeId) {
+      return true;
+    }
+  }
+  return false;
+}
+
+void createBinLib(FILE *libFp, const char *sym, const char *op, int type) {
+  if (!hasType(type)) {
     fprintf(libFp, "defproc func_%s(a, b, c) {\n", sym);
     fprintf(libFp, "  chp {\n    *[a?x, b?y; c!(x%sy) ]\n  }\n}\n\n", op);
   }
 }
 
-void createUniLib(FILE *libFp, const char *sym, const char *op) {
-  String symbol(sym);
-  if (std::find(createdProcs.begin(), createdProcs.end(), symbol)
-      == createdProcs.end()) {
-    createdProcs.push_back(symbol);
+void createUniLib(FILE *libFp, const char *sym, const char *op, int type) {
+  if (!hasType(type)) {
     fprintf(libFp, "defproc func_%s(a, b) {\n", sym);
     fprintf(libFp, "  chp {\n    *[a?x; b!(%sx) ]\n  }\n}\n\n", op);
   }
 }
 
 void createMerge(FILE *libFp) {
-  String symbol = "merge";
-  if (std::find(createdProcs.begin(), createdProcs.end(), symbol)
-      == createdProcs.end()) {
-    createdProcs.push_back(symbol);
+  if (!hasType(ACT_DFLOW_MERGE)) {
     fprintf(libFp, "defproc control_merge(ctrl, lIn, rIn, out) {\n");
     fprintf(libFp, "  chp {\n  }\n}\n\n");
   }
 }
 
 void createSplit(FILE *libFp) {
-  String symbol = "split";
-  if (std::find(createdProcs.begin(), createdProcs.end(), symbol)
-      == createdProcs.end()) {
-    createdProcs.push_back(symbol);
+  if (!hasType(ACT_DFLOW_SPLIT)) {
     fprintf(libFp, "defproc control_split(ctrl, in, lOut, rOut) {\n");
     fprintf(libFp, "  chp {\n  }\n}\n\n");
   }
 }
 
 void createSource(FILE *libFp) {
-  String symbol = "source";
-  if (std::find(createdProcs.begin(), createdProcs.end(), symbol)
-      == createdProcs.end()) {
-    createdProcs.push_back(symbol);
+  if (!hasType(E_INT)) {
     fprintf(libFp, "defproc source(in, out) {\n");
     fprintf(libFp, "  chp {\n  }\n}\n\n");
   }
 }
 
 void createBuff(FILE *libFp) {
-  String symbol = "buff";
-  if (std::find(createdProcs.begin(), createdProcs.end(), symbol)
-      == createdProcs.end()) {
-    createdProcs.push_back(symbol);
+  if (!hasType(E_VAR)) {
     fprintf(libFp, "defproc buff(in, out) {\n");
     fprintf(libFp, "  chp {\n  }\n}\n\n");
   }
