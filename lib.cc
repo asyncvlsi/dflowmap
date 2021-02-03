43ed9f44d5b88e9cc0e5e5b8b9963114ca922c50
Rui Li <rui.lee92@gmail.com>
2021-02-03 09:51:22 -0500

    Can simulate simple arithmetic act file.
    Add one more arithmetic benchmark: arith.act.

--- lib.cc
+++ lib.cc
@@ -3,9 +3,23 @@
 
 #define MAX_OP_TYPE_NUM 10
 #define MAX_EXPR_TYPE_NUM 100
+#define MAX_SOURCE_VAL_NUM 100
 
 int opTypes[MAX_OP_TYPE_NUM];
 int exprTypes[MAX_EXPR_TYPE_NUM];
+unsigned sourceVals[MAX_SOURCE_VAL_NUM];
+
+bool hasSourceVal(unsigned sourceVal) {
+  for (unsigned i = 0; i < MAX_SOURCE_VAL_NUM; i++) {
+    if (sourceVals[i] == UINT32_MAX) {
+      sourceVals[i] = sourceVal;
+      return false;
+    } else if (sourceVals[i] == sourceVal) {
+      return true;
+    }
+  }
+  fatal_error("We have reached the end of sourceVals array!\n");
+}
 
 bool hasExpr(int typeId) {
   for (unsigned i = 0; i < MAX_EXPR_TYPE_NUM; i++) {
@@ -31,48 +45,79 @@ bool hasOp(int typeId) {
   fatal_error("We have reached the end of opTypes array!\n");
 }
 
-void createBinLib(FILE *libFp, const char *sym, const char *op, int type) {
+void createBinLib(FILE *libFp,
+                  const char *sym,
+                  const char *op,
+                  int type) {
   if (!hasExpr(type)) {
-    fprintf(libFp, "defproc func_%s(a, b, c) {\n", sym);
-    fprintf(libFp, "  chp {\n    *[a?x, b?y; c!(x%sy) ]\n  }\n}\n\n", op);
+    fprintf(libFp,
+            "defproc func_%s(chan?(int) a, b; chan!(int) c) {\n",
+            sym);
+    fprintf(libFp, "  int x, y;\n");
+    fprintf(libFp,
+            "  chp {\n    *[a?x, b?y; c!(x%sy) ]\n  }\n}\n\n",
+            op);
   }
 }
 
-void createUniLib(FILE *libFp, const char *sym, const char *op, int type) {
+void createUniLib(FILE *libFp,
+                  const char *sym,
+                  const char *op,
+                  int type) {
   if (!hasExpr(type)) {
-    fprintf(libFp, "defproc func_%s(a, b) {\n", sym);
+    fprintf(libFp,
+            "defproc func_%s(chan?(int)a; chan!(int) b) {\n",
+            sym);
+    fprintf(libFp, "  int x;\n");
     fprintf(libFp, "  chp {\n    *[a?x; b!(%sx) ]\n  }\n}\n\n", op);
   }
 }
 
 void createMerge(FILE *libFp) {
   if (!hasOp(ACT_DFLOW_MERGE)) {
-    fprintf(libFp, "defproc control_merge(ctrl, lIn, rIn, out) {\n");
+    fprintf(libFp,
+            "defproc control_merge(chan?(bool)ctrl, chan?(int)lIn, rIn; chan!(int) out) {\n");
+    fprintf(libFp, "  int x;\n");
     fprintf(libFp, "  chp {\n");
-    fprintf(libFp, "    [~ctrl -> out!lIn [] ctrl -> out!rIn]\n");
+    fprintf(libFp, "    *[~ctrl -> lIn?x [] ctrl -> rIn?x; out!x]\n");
     fprintf(libFp, "  }\n}\n\n");
   }
 }
 
 void createSplit(FILE *libFp) {
   if (!hasOp(ACT_DFLOW_SPLIT)) {
-    fprintf(libFp, "defproc control_split(ctrl, in, lOut, rOut) {\n");
+    fprintf(libFp,
+            "defproc control_split(chan?(bool)ctrl, chan?(int)in; chan!(int) lOut, rOut) {\n");
+    fprintf(libFp, "  int x;\n");
     fprintf(libFp, "  chp {\n");
-    fprintf(libFp, "    [~ctrl -> lOut!in [] ctrl -> rOut!in]\n");
+    fprintf(libFp,
+            "    *[in?x; ~ctrl -> lOut!x [] ctrl -> rOut!x]\n");
     fprintf(libFp, "  }\n}\n\n");
   }
 }
 
-void createSource(FILE *libFp) {
-  if (!hasExpr(E_INT)) {
-    fprintf(libFp, "defproc source(in, out) {\n");
-    fprintf(libFp, "  chp {\n  }\n}\n\n");
+void createSource(FILE *libFp, unsigned sourceVal) {
+  if (!hasSourceVal(sourceVal)) {
+    fprintf(libFp, "defproc source%u(chan!(int)x) {\n", sourceVal);
+    fprintf(libFp, "  chp {\n");
+    fprintf(libFp, "    *[x!%u]\n", sourceVal);
+    fprintf(libFp, "  }\n}\n\n");
   }
 }
 
 void createBuff(FILE *libFp) {
   if (!hasExpr(E_VAR)) {
-    fprintf(libFp, "defproc buff(in, out) {\n");
-    fprintf(libFp, "  chp {\n    out!in\n  }\n}\n\n");
+    fprintf(libFp,
+            "defproc buffer(chan?(int)in; chan!(int) out) {\n");
+    fprintf(libFp, "  int x;\n");
+    fprintf(libFp, "  chp {\n    *[in?x;out!x]\n  }\n}\n\n");
   }
+}
+
+void createSink(FILE *libFp) {
+  fprintf(libFp, "defproc sink(chan?(int) x) {\n");
+  fprintf(libFp, "  int t;");
+  fprintf(libFp, "  chp {\n");
+  fprintf(libFp, "  *[x?t; log (\"got \", t)]\n");
+  fprintf(libFp, "  }\n}\n");
 }
