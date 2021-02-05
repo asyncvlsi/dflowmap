cdc36d7ee1f90aa3d66ee052af8eca3f9be2e24b
Rui Li <rui.lee92@gmail.com>
2021-02-05 01:20:44 -0500

    update for handling op's bitwidth info.

--- lib.cc
+++ lib.cc
@@ -5,8 +5,14 @@
 #define MAX_EXPR_TYPE_NUM 100
 #define MAX_SOURCE_VAL_NUM 100
 
-int opTypes[MAX_OP_TYPE_NUM];
-int exprTypes[MAX_EXPR_TYPE_NUM];
+//int opTypes[MAX_OP_TYPE_NUM];
+//int exprTypes[MAX_EXPR_TYPE_NUM];
+
+/* opType, bitwidth */
+std::pair<int, int> opTypePairs[MAX_SOURCE_VAL_NUM];
+/* exprType, bitwidth */
+std::pair<int, int> exprTypePairs[MAX_SOURCE_VAL_NUM];
+/* sourceVal, bitwidth */
 std::pair<unsigned, int> sourcePairs[MAX_SOURCE_VAL_NUM];
 
 bool hasSourceVal(unsigned sourceVal, int bitwidth) {
@@ -22,36 +28,38 @@ bool hasSourceVal(unsigned sourceVal, int bitwidth) {
   fatal_error("We have reached the end of sourcePairs array!\n");
 }
 
-bool hasExpr(int typeId) {
+bool hasExpr(int typeId, int bitwidth) {
+  std::pair<int, int> exprPair = std::make_pair(typeId, bitwidth);
   for (unsigned i = 0; i < MAX_EXPR_TYPE_NUM; i++) {
-    if (exprTypes[i] == -1) {
-      exprTypes[i] = typeId;
+    if (exprTypePairs[i].first == INT32_MAX) {
+      exprTypePairs[i] = exprPair;
       return false;
-    } else if (exprTypes[i] == typeId) {
+    } else if (exprTypePairs[i] == exprPair) {
       return true;
     }
   }
-  fatal_error("We have reached the end of exprTypes array!\n");
+  fatal_error("We have reached the end of exprTypePairs array!\n");
 }
 
-bool hasOp(int typeId) {
+bool hasOp(int typeId, int bitwidth) {
+  std::pair<int, int> opPair = std::make_pair(typeId, bitwidth);
   for (unsigned i = 0; i < MAX_OP_TYPE_NUM; i++) {
-    if (opTypes[i] == -1) {
-      opTypes[i] = typeId;
+    if (opTypePairs[i].first == INT32_MAX) {
+      opTypePairs[i] = opPair;
       return false;
-    } else if (opTypes[i] == typeId) {
+    } else if (opTypePairs[i] == opPair) {
       return true;
     }
   }
-  fatal_error("We have reached the end of opTypes array!\n");
+  fatal_error("We have reached the end of opTypePairs array!\n");
 }
 
-void createBinLib(FILE *libFp, const char *sym, const char *op, int type,
+void createBinLib(FILE *libFp, const char *sym, const char *op, int typeId,
                   int inWidth, int outWidth) {
-  if (!hasExpr(type)) {
+  if (!hasExpr(typeId, inWidth)) {
     fprintf(libFp,
-            "defproc func_%s(chan?(int<%d>) a, b; chan!(int<%d>) c) {\n",
-            sym, inWidth, outWidth);
+            "defproc func_%s_%d(chan?(int<%d>) a, b; chan!(int<%d>) c) {\n",
+            sym, inWidth, inWidth, outWidth);
     fprintf(libFp, "  int<%d> x, y;\n", inWidth);
     fprintf(libFp,
             "  chp {\n    *[a?x, b?y; c!(x%sy) ]\n  }\n}\n\n",
@@ -59,21 +67,21 @@ void createBinLib(FILE *libFp, const char *sym, const char *op, int type,
   }
 }
 
-void createUniLib(FILE *libFp, const char *sym, const char *op, int type, int bitwidth) {
-  if (!hasExpr(type)) {
+void createUniLib(FILE *libFp, const char *sym, const char *op, int typeId, int bitwidth) {
+  if (!hasExpr(typeId, bitwidth)) {
     fprintf(libFp,
-            "defproc func_%s(chan?(int<%d>)a; chan!(int<%d>) b) {\n",
-            sym, bitwidth, bitwidth);
+            "defproc func_%s_%d(chan?(int<%d>)a; chan!(int<%d>) b) {\n",
+            sym, bitwidth, bitwidth, bitwidth);
     fprintf(libFp, "  int<%d> x;\n", bitwidth);
     fprintf(libFp, "  chp {\n    *[a?x; b!(%sx) ]\n  }\n}\n\n", op);
   }
 }
 
 void createMerge(FILE *libFp, int bitwidth) {
-  if (!hasOp(ACT_DFLOW_MERGE)) {
+  if (!hasOp(ACT_DFLOW_MERGE, bitwidth)) {
     fprintf(libFp,
-            "defproc control_merge(chan?(int<1>)ctrl; chan?(int<%d>)lIn, "
-            "rIn; chan!(int<%d>) out) {\n", bitwidth, bitwidth);
+            "defproc control_merge%d(chan?(int<1>)ctrl; chan?(int<%d>)lIn, "
+            "rIn; chan!(int<%d>) out) {\n", bitwidth, bitwidth, bitwidth);
     fprintf(libFp, "  int<%d> x;\n  bool c;\n", bitwidth);
     fprintf(libFp, "  chp {\n");
     fprintf(libFp,
@@ -83,10 +91,10 @@ void createMerge(FILE *libFp, int bitwidth) {
 }
 
 void createSplit(FILE *libFp, int bitwidth) {
-  if (!hasOp(ACT_DFLOW_SPLIT)) {
+  if (!hasOp(ACT_DFLOW_SPLIT, bitwidth)) {
     fprintf(libFp,
-            "defproc control_split(chan?(int<1>)ctrl; chan?(int<%d>)in; "
-            "chan!(int<%d>) lOut, rOut) {\n", bitwidth, bitwidth);
+            "defproc control_split%d(chan?(int<1>)ctrl; chan?(int<%d>)in; "
+            "chan!(int<%d>) lOut, rOut) {\n", bitwidth, bitwidth, bitwidth);
     fprintf(libFp, "  int<%d> x;\n  bool c;\n", bitwidth);
     fprintf(libFp, "  chp {\n");
     fprintf(libFp,
@@ -105,16 +113,17 @@ void createSource(FILE *libFp, unsigned sourceVal, int bitwidth) {
 }
 
 void createBuff(FILE *libFp, int bitwidth) {
-  if (!hasExpr(E_VAR)) {
+  if (!hasExpr(E_VAR, bitwidth)) {
     fprintf(libFp,
-            "defproc buffer(chan?(int<%d>)in; chan!(int<%d>) out) {\n", bitwidth, bitwidth);
-    fprintf(libFp, "  int<%d> x;\n", bitwidth);
+            "defproc buffer%d(chan?(int<%d>)in; chan!(int<%d>) out) {\n", bitwidth, bitwidth,
+            bitwidth);
+    fprintf(libFp, "  int<%d> x;\n", bitwidth, bitwidth);
     fprintf(libFp, "  chp {\n    *[in?x;out!x]\n  }\n}\n\n");
   }
 }
 
 void createSink(FILE *libFp, int bitwidth) {
-  fprintf(libFp, "defproc sink(chan?(int<%d>) x) {\n", bitwidth);
+  fprintf(libFp, "defproc sink%d(chan?(int<%d>) x) {\n", bitwidth, bitwidth);
   fprintf(libFp, "  int<%d> t;", bitwidth);
   fprintf(libFp, "  chp {\n");
   fprintf(libFp, "  *[x?t; log (\"got \", t)]\n");
