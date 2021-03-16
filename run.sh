3b9f2dfab43ae0d28a38f6d3838acae913a1cec4
RuiLi92 <rui.lee92@gmail.com>
2021-03-16 17:59:40 -0400

    quick backup.

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 clear
 mv ~/main.cc ./
 mv ~/lib.cc ./
-mv ~/fluid.act ./
+#mv ~/fluid.act ./
 mv ~/common.h ./
 make clean
 rm -f result_$1
@@ -10,6 +10,7 @@ make
 ./dflowmap.* $1
 cp result_$1 ~
 cp lib_$1 ~
+cp conf_$1 ~
 cp $1 ~
 actsim -cnf=conf_$1 result_$1 "main<>"
 
