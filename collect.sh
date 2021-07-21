82a03eeb83c0eb31922a87aa8adefc07fe48a2da
RuiLi92 <rui.lee92@gmail.com>
2021-07-21 11:00:24 -0400

    Add log analysis code.

--- collect.sh
+++ collect.sh
@@ -1,15 +1,15 @@
-LOC=rl538@grace.hpc.yale.edu:/gpfs/loomis/project/manohar/rl538/dflowmap
+LOC=rl538@grace.hpc.yale.edu:/gpfs/loomis/project/manohar/rl538/dflowmap$2
 scp $LOC/lib_$1 ./
 scp $LOC/result_$1 ./
 scp $LOC/conf_$1 ./
 scp $LOC/$1 ./
 #scp $LOC/output ./
-scp $LOC/$1_statistics ./
-scp $LOC/$1_merge ./
-scp $LOC/$1_split ./
-scp $LOC/$1_mem ./
-scp $LOC/$1_energy ./
-scp $LOC/$1_fu ./
+#scp $LOC/$1_statistics ./
+#scp $LOC/$1_merge ./
+#scp $LOC/$1_split ./
+#scp $LOC/$1_mem ./
+#scp $LOC/$1_energy ./
+#scp $LOC/$1_fu ./
 #cp lib_$1 lib_2_$1
 #cp result_$1 result_2_$1
 #cp conf_$1 conf_2_$1
